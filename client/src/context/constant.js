0f290307e95d5b3f088fdbe63a8d484958ed6713
htdat0704 <htdat.18it3@sict.udn.vn>
2022-08-01 14:22:52 +0700

    cm

--- client/src/context/constant.js
+++ client/src/context/constant.js
@@ -5,5 +5,5 @@ export const apiUrl =
         : 'https://agile-reaches-17285.herokuapp.com'
 
 // export const apiUrlHeroku = 'https://agile-reaches-17285.herokuapp.com'
-// export const apiUrlHeroku = 'http://localhost:4000'
-export const apiUrlHeroku = 'https://enigmatic-fortress-98177.herokuapp.com'
+export const apiUrlHeroku = 'http://localhost:4000'
+// export const apiUrlHeroku = 'https://enigmatic-fortress-98177.herokuapp.com'
